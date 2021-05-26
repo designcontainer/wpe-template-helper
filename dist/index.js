1711d86307b3f5d67e88a52208c513b86a0204bf
RostiMelk <hello@rosti.no>
2021-05-26 15:49:07 +0200

    remove broken dir

--- dist/index.js
+++ dist/index.js
@@ -9841,14 +9841,6 @@ module.exports = require("tty");
 
 /***/ }),
 
-/***/ 869:
-/***/ (function(module) {
-
-module.exports = eval("require")("./api-calls");
-
-
-/***/ }),
-
 /***/ 876:
 /***/ (function(__unusedmodule, exports, __webpack_require__) {
 
@@ -10255,115 +10247,22 @@ module.exports = { stringify, stripBom }
 /***/ }),
 
 /***/ 918:
-/***/ (function(module, __unusedexports, __webpack_require__) {
+/***/ (function(module) {
 
-const { copy } = __webpack_require__(630);
-const { readdir } = __webpack_require__(747).promises;
-const path = __webpack_require__(622);
-const core = __webpack_require__(186);
-const { getCommitFiles } = __webpack_require__(869);
-
-module.exports = { copyChangedFiles, parseCommaList, getBranchName, getListOfFilesToReplicate, getAuthanticatedUrl, isInit };
-
-/**
- * @param  {Object} octokit GitHub API client instance
- * @param  {Object} commitId Id of the commit to check for files changes
- * @param  {String} owner org or user name
- * @param  {String} repo repo name
- * @param  {String} filesToIgnore comma-separated list of files that should be ignored
- * @param  {String} triggerEventName name of the event that triggered the workflow
- * 
- * @returns {Array<String>} list of filepaths of modified files
- */
-async function getListOfFilesToReplicate(octokit, commitId, owner, repo, filesToIgnore, triggerEventName) {
-  let filesToCheckForReplication;
-  const defaultWorkflowsDir = '.github/workflows';
-
-  if (triggerEventName === 'push') {
-    const commitFiles = await getCommitFiles(octokit, commitId, owner, repo);
-    filesToCheckForReplication = commitFiles.map((el) => el.filename);
-    core.debug(`DEBUG: list of commited files for commit ${commitId} that is used to check if there was any file located in .github/workflows modified: ${filesToCheckForReplication}`);
-  }
-
-  if (triggerEventName === 'workflow_dispatch') {
-    const workflowDirPath = path.join(process.cwd(), defaultWorkflowsDir);
-    const workflowDirFilesList = await readdir(workflowDirPath);
-    filesToCheckForReplication = workflowDirFilesList.map(filename => path.join(defaultWorkflowsDir, filename));
-    core.debug(`DEBUG: list of files from ${workflowDirPath} directory is ${filesToCheckForReplication}`);
-  }
-
-  const changedFiles = [];
-  const ignoreFilesList = filesToIgnore ? parseCommaList(filesToIgnore) : [];
-  
-  core.info(`List of files that should be ignored: ${ignoreFilesList}`);
-
-  for (const filename of filesToCheckForReplication) {
-    const onlyFileName = filename.split('/').slice(-1)[0];
-    const isFileIgnored = !!ignoreFilesList.map(file => file === onlyFileName).filter(Boolean).length;
-    //TODO for now this action is hardcoded to only monitor changes from .github/workflows directory because it is supposed to support global workflows and no other files
-    const isWorkflowFile = filename.includes(defaultWorkflowsDir);
-    core.info(`Checking if ${filename} is located in workflows directory (${isWorkflowFile}) and if ${onlyFileName} should be ignored (${isFileIgnored})`);
-
-    if (isWorkflowFile && !isFileIgnored) {
-      changedFiles.push(filename);
-    }
-  }
-
-  return changedFiles;
-}
-
-/**
- * @param  {Array} filesList list of files that need to be copied
- * @param  {String} destination where file should be copied
- */
-async function copyChangedFiles(filesList, destination) {
-  await Promise.all(filesList.map(async filepath => {
-    return await copy(path.join(process.cwd(), filepath), path.join(destination, filepath));
-  }));
-}
-
-/**
- * @param  {String} list names of values that can be separated by comma
- * @returns  {Array<String>} input names not separated by string but as separate array items
- */
-function parseCommaList(list) {
-  return list.split(',').map(i => i.trim().replace(/['"]+/g, ''));
-}
-
-/**
- * Create a branch name. 
- * If commitId is not provided then it means action was not triggered by push and name must have some generated number and indicate manual run
- * 
- * @param  {String} commitId id of commit that should be added to branch name for better debugging of changes
- * @returns  {String}
- */
-function getBranchName(commitId) {
-  return commitId ? `bot/update-global-workflow-${commitId}` : `bot/manual-update-global-workflow-${Math.random().toString(36).substring(7)}`;
-}
+module.exports = { getAuthanticatedUrl };
 
 /**
  * Creates a url with authentication token in it
- * 
+ *
  * @param  {String} token access token to GitHub
  * @param  {String} url repo URL
  * @returns  {String}
  */
 function getAuthanticatedUrl(token, url) {
-  const arr = url.split('//');
-  return `https://${token}@${arr[arr.length - 1]}.git`;
-};
-
-/**
- * Checking if repo is initialized cause if it isn't we need to ignore it
- * 
- * @param  {Array<Object>} branches list of all local branches with detail info about them
- * @param  {String} defaultBranch name of default branch that is always set even if repo not initialized
- * @returns  {Boolean}
- */
-function isInit(branches, defaultBranch) {
-  core.debug(`DEBUG: list of local branches: ${branches.branches}`);
-  return !!branches.branches[defaultBranch];
+	const arr = url.split('//');
+	return `https://${token}@${arr[arr.length - 1]}.git`;
 }
+
 
 /***/ }),
 
